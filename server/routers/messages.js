f0648723ab28dae5a049b64987b43ca444a7f9db
liemnt <>
2022-05-11 14:21:39 +0700

    dasdas

--- server/routers/messages.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getMessagesListRoom } from "../controllers/messageControllers.js";
-const router = express.Router();
-
-router.get("/list/:room_id", getMessagesListRoom);
-
-export default router;
--- server/routers/user.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { loginUser } from "../controllers/user.js";
-const router = express.Router();
-
-router.post("/login", loginUser);
-
-export default router;
